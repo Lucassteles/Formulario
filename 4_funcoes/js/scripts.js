7cb27562bb296a2aa32437f1fda159766dacbcaa
Lucas Teles <lucas_teles500@hotmail.com>
2026-01-21 20:52:21 -0300

    Atualizando javascript

---
+++ 4_funcoes/js/scripts.js
@@ -0,0 +1,230 @@
+// 1- criando uma função 
+
+// function minhaFuncao() { 
+//     console.log("testando");
+// } 
+
+// minhaFuncao(); 
+// minhaFuncao(); 
+
+// const minhaFuncaoEmVariavel = function () { 
+//     console.log("Função em variavel");
+// };
+
+// minhaFuncaoEmVariavel(); 
+// function funcaoComParametro(txt) {
+//     console.log(`Imprimindo: ${txt}`);
+// } 
+
+// funcaoComParametro("Imprimindo alguma coisa"); 
+
+// funcaoComParametro("outra função"); 
+
+// // 2 - return 
+// const a = 10;
+// const b = 20;
+// const c = 30;
+// const d = 40; 
+
+// function soma (n1,n2) { 
+//     return n1 + n2;
+// } 
+
+// const resultado = soma(a,b); 
+
+// console.log(resultado); 
+
+// console.log(soma(c,d));  
+
+
+// // 3 - escopo da função 
+
+// let y = 10; 
+ 
+// function testandoEscopo() { 
+//     let y = 20;
+//     console.log(`Y dentro da função e: ${y}`);
+// } 
+
+// testandoEscopo(); 
+
+// y=15;
+
+// console.log(`Y fora da função é: ${y}`); 
+
+// testandoEscopo();
+
+// // 4 - escopo aninhado
+// let m = 10;
+
+// function escopoAninhado() {
+//     let m = 10;
+
+//     function escopoInterno() {
+//         let m = 20;
+
+//         if (true) {
+//             let m = 30;
+
+//             if (true) {
+//                 let m = 40;
+//                 console.log(m); // 40
+//             }
+
+//             console.log(m); // 30
+//         }
+
+//         console.log(m); // 20
+//     }
+
+//     escopoInterno();
+//     console.log(m); // 10
+// }
+
+// escopoAninhado();
+
+// console.log(m);  
+
+// 5- arrow function 
+// const testeArrow = () => { 
+//     console.log("Está é uma arrow function");
+// }; 
+// testeArrow(); 
+
+// const parOuImpar = (n) => { 
+//     if (n % 2=== 0) { 
+//         console.log("Par"); 
+//         return;
+//     } 
+//     console.log("Impar");
+// }; 
+
+// parOuImpar(5); 
+
+// parOuImpar(10); 
+
+// 6- mais sobre arrow funcitions 
+
+// const raizQuadrada = (x) =>{ 
+//     return x * x;
+// }; 
+
+// console.log(raizQuadrada(4));  
+
+// const raizQuadrada2 = (x) =>  x * x; 
+
+// console.log(raizQuadrada2(5)); 
+
+// console.log(raizQuadrada(12)); 
+
+// const helloworld = () => console.log("Hello world"); 
+
+// console.log(); 
+
+
+//7- Parametro opcional 
+
+//const multiplication = function (m,n){ 
+
+    //if(m === undefined){ 
+    //    return m * 2 
+   // } else { 
+ //return m * 2;
+    //}
+    //};
+
+    //console.log(multiplication(5)); 
+
+    //console.log(multiplication (2,4)); 
+
+   // const greeting = (name) => { 
+  // if(name == false) { 
+   // console.log("olá!") 
+   // return;
+   //}
+  //console.log(`Olá ${name}!`);
+    //}; 
+
+    //greeting(); 
+
+    //greeting("Matheus");
+
+
+ // 8 - valor default 
+
+ const customGreeting = (name, greet = "Olá") => { 
+    return `${greet}, ${name}!`;
+};
+
+console.log(customGreeting("Matheus"));
+console.log(customGreeting("João", "Bom dia"));
+
+const repeatText = (text, repeat = 2) => { 
+    for (let i = 0; i < repeat; i++) { 
+        console.log(text);
+    }
+};
+
+repeatText("Testando");
+repeatText("Agora repete 5 vezes", 5); 
+
+// 9 - closure 
+function someFunction () { 
+    let txt="Alguma coisa" 
+
+    function display() { 
+        console.log(txt)
+    }
+    display();
+} 
+
+someFunction(); 
+
+
+
+// 10- mais sobre closure 
+const multiplicationClosure = (n) => { 
+    return(m) => { 
+        return n * m 
+    };
+};
+ 
+
+const c1= multiplicationClosure(5); 
+const c2= multiplicationClosure(10); 
+
+console.log(c1); 
+
+console.log(c2); 
+console.log(c1(5)); 
+
+console.log(c2(10)); 
+
+// 11- Recursion 
+
+const untilTen = (n,m) => { 
+    if(n < 10) { 
+        console.log("A função parou  de executar !"); 
+    }else{ 
+        const x = n - m; 
+         
+        console.log(x); 
+
+        untilTen(x,m);
+
+    }
+}; 
+
+untilTen(100,7); 
+
+function factorial (x) { 
+    if(x === 0) { 
+        return 1;
+    } else { 
+ return x * factorial (x - 1 );
+    }
+}
+
+const num = 6; 
+const result = factorial(num); 
+console.log(`O fatorial do número ${num} é ${result}`);
